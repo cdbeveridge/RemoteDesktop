8b94891e29b9dafe10671db6dc2aa394ef9c2242
cdbeveridge <43109502+cdbeveridge@users.noreply.github.com>
2018-09-09 14:07:40 +0100

    initial B2 commit

--- RemoteDesktop/Program.cs
+++ RemoteDesktop/Program.cs
@@ -6,20 +6,46 @@ namespace RemoteDesktop
     {
         static void Main(string[] args)
         {
-            RegistryKey fDeny = Registry.CurrentUser.OpenSubKey(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server", true);
-            RegistryKey userAuth = Registry.CurrentUser.OpenSubKey(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp", true);
+            bool AllowRemoteAssistance = true;
+            int RemoteDesktopSelectNumber = 2;
+            RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+            key = key.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Remote Assistance", true);
+            if (AllowRemoteAssistance)
+                key.SetValue("fAllowToGetHelp", 1, RegistryValueKind.DWord);
+            else
+                key.SetValue("fAllowToGetHelp", 0, RegistryValueKind.DWord);
+            key.Flush();
+            if (key != null)
+                key.Close();
 
-            if (fDeny != null)
+            if (RemoteDesktopSelectNumber == 1 || RemoteDesktopSelectNumber == 2)
             {
-                fDeny.SetValue("fDenyTSConnections", 0, RegistryValueKind.DWord);
-            }
+                key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+                key = key.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\", true);
+                key.SetValue("UserAuthentication", 0, RegistryValueKind.DWord);
+                key.Flush();
+                if (key != null)
+                    key.Close();
 
-            if (userAuth != null)
+                key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+                key = key.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server", true);
+                if (RemoteDesktopSelectNumber == 1)
+                    key.SetValue("fDenyTSConnections", 1, RegistryValueKind.DWord);
+                else
+                    key.SetValue("fDenyTSConnections", 0, RegistryValueKind.DWord);
+                key.Flush();
+                if (key != null)
+                    key.Close();
+            }
+            else if (RemoteDesktopSelectNumber == 3)
             {
-                userAuth.SetValue("UserAuthentication", 1, RegistryValueKind.DWord);
+                key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+                key = key.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\", true);
+                key.SetValue("UserAuthentication", 1, RegistryValueKind.DWord);
+                key.Flush();
+                if (key != null)
+                    key.Close();
             }
-
-
         }
     }
 }
