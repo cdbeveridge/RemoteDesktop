edcfbaa6b5abb7349807f9eed073e8256646ff90
cdbeveridge <43109502+cdbeveridge@users.noreply.github.com>
2018-09-09 11:18:56 +0100

    Initial commit

---
+++ RemoteDesktop/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+
+namespace RemoteDesktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey fDeny = Registry.CurrentUser.OpenSubKey(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server", true);
+            RegistryKey userAuth = Registry.CurrentUser.OpenSubKey(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp", true);
+
+            if (fDeny != null)
+            {
+                fDeny.SetValue("fDenyTSConnections", 0);
+            }
+
+            if (userAuth != null)
+            {
+                userAuth.SetValue("UserAuthentication", 1);
+            }
+            
+
+        }
+    }
+}
